cb1dc87fec87846cc9cd26346eac3c15bafc5157
Punchks <jhordylucascalla@gmail.com>
2026-04-10 10:23:41 -0500

    Subiendo e1

---
+++ SE3-C/E1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE3_C
+{
+    internal class E1
+    {
+        static void Main(string[] args)
+        {
+            string n, c;
+            Console.WriteLine("Ingrese Nombre:");
+            n=Console.ReadLine();
+            Console.WriteLine("Ingrese carrera:");
+            c = Console.ReadLine();
+
+            Console.WriteLine($"{n}, Bienvenido al curso de Fundamentos de Algoritmos de la carrera {c}");
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
